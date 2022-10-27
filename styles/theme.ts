445d4ea9aa5d73dde5b4259b7d8aef96d8280a6e
Eriquy Jhordan <eriquy@gmail.com>
2022-10-27 18:58:48 -0300

    feat: create a theme and global styles

---
+++ styles/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    main: '#084F8C',
+    secondary: '#44AAD3',
+    terciary: '#2C75BB',
+    shape: '#E6E6E6',
+    text_header: '#343A40',
+    text_body: '#495057',
+    text_subtitle: '#495057',
+  },
+};
+
+export default theme;
