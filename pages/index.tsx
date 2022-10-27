a849d73483fdfd678a83e297a60d31c368a98998
Eriquy Jhordan <eriquy@gmail.com>
2022-10-27 18:10:35 -0300

    fix: eslint and prettier fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { HomeContainer, HomeTitle } from '../styles/home.styled'
+import { HomeContainer, HomeTitle } from '../styles/home.styled';
 
 export default function Home() {
   return (
     <HomeContainer>
       <HomeTitle>Start a new project</HomeTitle>
     </HomeContainer>
-  )
+  );
 }
