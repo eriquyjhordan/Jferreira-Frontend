961bd69bad2c84208526ff27044eecac0a790716
Eriquy Jhordan <eriquy@gmail.com>
2022-10-27 17:58:31 -0300

    chore: add styled components

---
+++ styles/home.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HomeTitle = styled.h1`
+  font-size: 48px;
+  color: #333;
+`;
